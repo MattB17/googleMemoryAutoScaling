99ab72577618e31c7dbb10ce0645a5596734782d
MattB17 <matt.buckley1755@gmail.com>
2021-02-24 09:35:07 -0500

    Setting up TraceGenerator package

---
+++ TraceGenerator/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+# Get current directory and long description from README
+current_dir = path.abspath(path.dirname(__file__))
+
+# the setup
+setup(
+    name="TraceGenerator",
+    version="0.1.0",
+    description="Trace Generation for Memory Auto Scaling",
+    author="Matt Buckley",
+    packages=find_packages(exclude=['contrib', 'docs', 'tests'])
+)
