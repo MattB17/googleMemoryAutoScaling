59e10ae285a54419fa1ee1d7712843fee236e409
MattB17 <matt.buckley1755@gmail.com>
2021-02-24 16:11:43 -0500

    Adding ability to generate traces with periodicity

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ current_dir = path.abspath(path.dirname(__file__))
 
 # the setup
 setup(
-    name="TraceGeneration",
+    name="MemoryAutoScaling",
     version="0.1.0",
-    description="Trace Generation for Memory Auto Scaling",
+    description="Prediction Methods for Memory Auto Scaling",
     author="Matt Buckley",
     packages=find_packages(exclude=['contrib', 'docs', 'tests'])
 )
