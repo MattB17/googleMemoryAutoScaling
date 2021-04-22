f9463024608e13687b16b2811578c2a37fa91257
MattB17 <matt.buckley1755@gmail.com>
2021-04-22 12:46:49 -0400

    Fixing ARIMA model parameters

--- Scripts/buildARIMAModels.py
+++ Scripts/buildARIMAModels.py
@@ -9,9 +9,14 @@ from MemoryAutoScaling import analysis, specs, utils
 from MemoryAutoScaling.Models.Statistical import TraceARIMA
 
 
-ARIMA_p = [p for p in range(4)]
+ARIMA_p = [p for p in range(5)]
 ARIMA_d = [d for d in range(2)]
-ARIMA_q = [q for q in range(4)]
+ARIMA_q = [q for q in range(5)]
+
+MA_PARAMS = [(0, 0, q) for q in ARIMA_q[1:]]
+AR_PARAMS = [(p, 0, 0) for p in ARIMA_p[1:]]
+ARIMA_PARAMS = [arima_tup for arima_tup in product(ARIMA_p, ARIMA_d, ARIMA_q)
+                if arima_tup not in MA_PARAMS + AR_PARAMS + [(0, 0, 0)]]
 
 
 def build_arima_models_for_traces(traces_lst, results_dict,
@@ -19,7 +24,7 @@ def build_arima_models_for_traces(traces_lst, results_dict,
     arima_params_lst = [{'train_prop': train_prop,
                         'p': p, 'd': d, 'q': q,
                         'max_mem': max_mem}
-                        for p, d, q in product(ARIMA_p, ARIMA_d, ARIMA_q)]
+                        for p, d, q in ARIMA_PARAMS]
     analysis.get_best_model_results_for_traces(
         TraceARIMA, arima_params_lst, traces_lst,
         results_dict, specs.MODELS_COUNT)
