Fixing calculation of harvest proportions

MemoryAutoScaling/Models/Sequential/SequentialModel.py
@@ -111,34 +111,23 @@ class SequentialModel(TraceModel):
         return trace.get_spare_resource_in_window(
             self.get_target_variable(), train_thresh, test_thresh)
 
-    def get_available_resource_data(self, trace, tuning=True):
-        """A time series of the available resource for `trace`.
-
-        The time series is restricted to the evaluation interval specified
-        by `tuning`. If `tuning` is True then the model is being tuned so the
-        time series is restricted to the validation set. Otherwise, it is
-        restricted to the testing set.
+    def get_allocated_resource_amount(self, trace):
+        """The amount of the target resource allocated for `trace`.
 
         Parameters
         ----------
         trace: Trace
-            The `Trace` object from which the available resource numbers are
-            retrieved.
-        tuning: bool
-            A boolean value indicating whether or not the model is being
-            tuned.
+            The `Trace` object from which the resource number is retrieved.
 
         Returns
         -------
-        np.array
-            A numpy array representing the amount of the resource available
-            for each time point in the evaluation window specified by `tuning`.
+        float
+            A float representing the amount of the target resource allocated
+            to `trace` over its duration.
 
         """
-        total_avail_ts = trace.get_target_availability_time_series(
+        return trace.get_amount_allocated_for_target(
             self.get_target_variable())
-        _, avail_ts = self.split_data(total_avail_ts, tuning)
-        return avail_ts
 
     def get_model_data_for_trace(self, trace):
         """Gets the data for modeling from `trace`.
@@ -209,9 +198,9 @@ class SequentialModel(TraceModel):
         y_train, y_test = self.split_data(trace_ts, tuning)
         total_spare = self.get_total_spare(trace, tuning)
         preds_train, preds_test = self._get_predictions(trace_ts, tuning)
-        avail_ts = self.get_available_resource_data(trace, tuning)
+        avail_val = self.get_allocated_resource_amount(trace)
         return ModelResults.from_data(
-            self.get_params(), avail_ts, y_train,
+            self.get_params(), avail_val, y_train,
             preds_train, y_test, preds_test, total_spare)
 
     def plot_trace_vs_prediction(self, trace, tuning=True):

MemoryAutoScaling/utils.py
@@ -914,13 +914,13 @@ def calculate_harvest_stats(avail_val, actual_ts, predicted_ts, buffer_pct):
         harvested and the proportion of violations.
 
     """
-    actual_spare_pcts = avail_val - actual_ts
-    buffered_pred_ts = np.minimum(predicted_ts * (1 + buffer_pct), avail_val)
-    predicted_spare_pcts = avail_val - buffered_pred_ts
-    under_pred_indices = buffered_pred_ts < actual_ts
-    predicted_spare_pcts[under_pred_indices] = 0
-    actual_spare = np.sum(actual_spare_pcts * avail_val)
-    harvested_spare = np.sum(predicted_spare_pcts * avail_val)
-    prop_harvested = harvested_spare / actual_spare
-    prop_violations = np.sum(under_pred_indices) / len(actual_ts)
+    actuals = actual_ts * avail_val
+    predicteds = np.minimum(predicted_ts * avail_val * (1.0 + buffer_pct),
+                            avail_val)
+    actual_spare_ts = avail_val - actuals
+    predicted_spare_ts = avail_val - predicteds
+    under_pred_indices = predicteds < actuals
+    predicted_spare_ts[under_pred_indices] = 0.0
+    prop_harvested = np.sum(predicted_spare_ts) / np.sum(actual_spare_ts)
+    prop_violations = np.sum(under_pred_indices) / len(actuals)
     return prop_harvested, prop_violations