3568dc4fc484771608b3accb622764f0a41380cc
MattB17 <matt.buckley1755@gmail.com>
2021-05-06 11:34:50 -0400

    Fixing bug in univariate script

--- Scripts/Univariate/buildUnivariateModels.py
+++ Scripts/Univariate/buildUnivariateModels.py
@@ -99,7 +99,7 @@ def build_reg_models_for_traces(trace_lst, results_dict,
         results_dict, specs.MODELS_COUNT, fixed_model_params)
 
 
-REG_VALS = [0.01, 0.03, 0.1, 0.3, 1.0, 3.0]
+SVM_VALS = [0.01, 0.03, 0.1, 0.3, 1.0, 3.0]
 
 
 def build_svm_models_for_traces(trace_lst, results_dict,
@@ -109,7 +109,7 @@ def build_svm_models_for_traces(trace_lst, results_dict,
         FEATURE_COLS, [target_col], train_prop, val_prop)
     fixed_model_params = {'data_handler': data_handler, 'lags': specs.LAGS}
     svm_params_lst = [{'reg_val': reg_val, **fixed_model_params}
-                       for reg_val in REG_VALS]
+                       for reg_val in SVM_VALS]
     analysis.get_best_model_results_for_traces(
         TraceSVM, svm_params_lst, trace_lst,
         results_dict, specs.MODELS_COUNT, fixed_model_params)
